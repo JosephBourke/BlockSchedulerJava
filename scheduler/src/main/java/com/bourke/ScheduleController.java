b17039df0682eb899ace91545457e46d9ca77c79
JosephBourke <bour3754@ravens.benedictine.edu>
2024-07-02 06:56:37 -0500

    Schedule Text Area Controller
    Added a controller method that allows for collection of data from the cells of the gridpane.

---
+++ scheduler/src/main/java/com/bourke/ScheduleController.java
@@ -0,0 +1,42 @@
+package com.bourke;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+
+
+
+public class ScheduleController {
+    private ArrayList<String> data= new ArrayList<>();
+
+    @FXML
+    private GridPane gp;
+
+
+    @FXML
+    public ArrayList<String> getScheduleData() throws IOException {
+        data = new ArrayList<>();
+        ObservableList<Node> list = gp.getChildren();
+
+
+        for (Node node : list) {
+            if(node instanceof TextArea){
+                TextArea ta = (TextArea) node;
+                data.add(ta.getText());                
+            }
+        }
+        System.out.println(data);
+        return data;
+    }
+
+
+
+}
