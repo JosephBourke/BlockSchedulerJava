f63e037d74a292c6acf2ee4bc7007378e028cdfa
JosephBourke <bour3754@ravens.benedictine.edu>
2024-07-03 11:05:23 -0500

    Load Data
    Successsfully loaded data on init of scene

--- scheduler/src/main/java/com/bourke/ScheduleController.java
+++ scheduler/src/main/java/com/bourke/ScheduleController.java
@@ -1,12 +1,15 @@
 package com.bourke;
 
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
@@ -14,7 +17,7 @@ import javafx.scene.layout.GridPane;
 
 
 
-public class ScheduleController {
+public class ScheduleController implements Initializable {
     private ArrayList<String> data= new ArrayList<>();
 
     @FXML
@@ -38,6 +41,25 @@ public class ScheduleController {
         fh.SaveObject(data, Constants.saveLocation);
         System.out.println(data);
         return data;
+    }
+
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        FileHandler fh = new FileHandler();
+        ArrayList<String> slist = (ArrayList<String>)fh.LoadObject(Constants.saveLocation);
+
+        ObservableList<Node> nlist = gp.getChildren();
+
+        int index = 0;
+        for (Node node : nlist) {
+            if(node instanceof TextArea){
+                TextArea ta = (TextArea) node;
+                ta.setText(slist.get(index));
+                index++;
+            }
+        }
+
     }
 
 
